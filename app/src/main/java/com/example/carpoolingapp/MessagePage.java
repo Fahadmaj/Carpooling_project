55001037cbf4356aaeae3942c534e04e61eec29b
Devin <devhze@yahoo.com>
2025-04-05 22:23:21 -0700

    Fixed the naming issue

--- app/src/main/java/com/example/carpoolingapp/MessagePage.java
+++ app/src/main/java/com/example/carpoolingapp/MessagePage.java
@@ -8,32 +8,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MessagePage extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_message_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        // need this for all activities
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             if (item.getItemId() == R.id.nav_home) {
 
                 if (!(getClass().equals(MainActivity.class))) {
                     // If not, start MainActivity using an Intent
-                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    Intent intent = new Intent(MessagePage.this, MainActivity.class);
                     startActivity(intent);
                 }
                 return true;
@@ -41,14 +38,14 @@ public class MainActivity extends AppCompatActivity {
                 // Check if we're already on MessagePage
                 if (!(getClass().equals(MessagePage.class))) {
                     // If not, start MessagePage using an Intent
-                    Intent intent = new Intent(MainActivity.this, MessagePage.class);
+                    Intent intent = new Intent(MessagePage.this, MessagePage.class);
                     startActivity(intent);
                 }
                 return true;
             } else if (item.getItemId() == R.id.nav_settings) {
                 if (!(getClass().equals(SettingPage.class))) {
                     // If not, start MessagePage using an Intent
-                    Intent intent = new Intent(MainActivity.this, SettingPage.class);
+                    Intent intent = new Intent(MessagePage.this, SettingPage.class);
                     startActivity(intent);
                 }
                 return true;
