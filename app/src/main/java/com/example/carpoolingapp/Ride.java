e9c8fc5abd88143f72cbfbd95c2da7da99876962
Devin <devhze@yahoo.com>
2025-04-06 15:48:26 -0700

    Created base UI and dummy logic for searching for rides

---
+++ app/src/main/java/com/example/carpoolingapp/Ride.java
@@ -0,0 +1,12 @@
+package com.example.carpoolingapp;
+
+public class Ride {
+    public String driverName;
+    public String destination;
+
+    public Ride(String driverName, String destination) {
+        this.driverName = driverName;
+        this.destination = destination;
+    }
+}
+
